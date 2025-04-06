4cd36dd66b598c4d000249725ea77ef3d4285ed9
Prasann Trivedi <prasanntrivedi6263@gmail.com>
2025-04-06 22:34:23 +0530

    CORS V3

--- app.py
+++ app.py
@@ -4,19 +4,10 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://www.youtube.com"])
 
-@app.route('/predict', methods=['POST', 'OPTIONS'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'OPTIONS':
-        # Preflight request for CORS
-        response = app.make_default_options_response()
-        response.headers['Access-Control-Allow-Origin'] = 'https://www.youtube.com'
-        response.headers['Access-Control-Allow-Methods'] = 'POST'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        return response
-
-    # POST request logic
     data = request.get_json()
     comment = data.get("comment")
 
@@ -25,6 +16,7 @@ def predict():
 
     with open("tokenizer.pkl", "rb") as f:
         tokenizer = pickle.load(f)
+
     labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
     model = load_model('toxicity_model.h5')
     sequence = tokenizer.texts_to_sequences([comment])
@@ -32,10 +24,7 @@ def predict():
     prediction = model.predict(padded)[0]
     result = {label: float(f"{prob:.3f}") for label, prob in zip(labels, prediction)}
 
-    response = jsonify({"comment": comment, "predictions": result})
-    response.headers['Access-Control-Allow-Origin'] = 'https://www.youtube.com'
-    return response
-
+    return jsonify({"comment": comment, "predictions": result})
 
 
 if __name__ == '__main__':
